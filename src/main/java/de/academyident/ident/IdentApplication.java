b5bf117d4960754e3b82a87d02ed83f07fd22eed
FlorianWeiss <git@florianweiss.dev>
2019-07-18 09:59:03 +0200

    Initial Commit

---
+++ src/main/java/de/academyident/ident/IdentApplication.java
@@ -0,0 +1,13 @@
+package de.academyident.ident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/academyident/ident/IdentApplicationTests.java
@@ -0,0 +1,16 @@
+package de.academyident.ident;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
