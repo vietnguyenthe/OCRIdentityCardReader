3c231f06f053b3be64c9049e9d945bc72ea937bb
viet_nguyenthe <the_viet_nguyen@yahoo.de>
2019-07-23 12:02:47 +0200

    Subbilder von allen relevanten Feldern erzeugbar

---
+++ src/main/java/de/academyident/ident/SubbildTester.java
@@ -0,0 +1,8 @@
+package de.academyident.ident;
+
+public class SubbildTester {
+    public static void main(String[] args) {
+        SubbildErsteller subbildErsteller = new SubbildErsteller();
+        subbildErsteller.erstelleAdresse();
+    }
+}
--- src/main/java/de/academyident/ident/TesseractTest.java
+++ src/main/java/de/academyident/ident/TesseractTest.java
@@ -6,15 +6,14 @@ import java.io.File;
 
 public class TesseractTest {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        String file = "src/main/resources/tesseract/Perso_Front.jpg";
+        String file = "src/main/resources/tesseract/geburtstag.jpg";
         Tesseract tesseract = new Tesseract();
         tesseract.setDatapath("src\\main\\resources\\tesseract");
-
         String fulltext = null;
 
-            fulltext = tesseract.doOCR(new File(file));
+        fulltext = tesseract.doOCR(new File(file));
 
         System.out.println(fulltext);
     }
