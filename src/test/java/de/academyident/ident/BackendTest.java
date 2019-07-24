1966ab5d9d99be95b57b168e6b75b89c4da9822c
viet_nguyenthe <the_viet_nguyen@yahoo.de>
2019-07-24 15:08:01 +0200

    Tests schreiben

--- src/test/java/de/academyident/ident/BackendTest.java
+++ src/test/java/de/academyident/ident/BackendTest.java
@@ -2,6 +2,7 @@ package de.academyident.ident;
 
 import de.academyident.ident.model.BundesDatenbank;
 import de.academyident.ident.model.Personendokument;
+import de.academyident.ident.util.SubbildErsteller;
 import de.academyident.ident.util.TesseractIdent;
 import de.academyident.ident.util.Validierung;
 import org.testng.Assert;
@@ -10,6 +11,7 @@ import org.testng.annotations.Test;
 
 import javax.validation.constraints.AssertFalse;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,20 +21,20 @@ public class BackendTest {
     List<BundesDatenbank> bundesDatenbankList = new ArrayList<>();
 
     @BeforeClass
-    public void bundesDatenbankListeErstellen(){
-        BundesDatenbank florianWeiss = new BundesDatenbank("Florian", "Weiss","blabla",
+    public void bundesDatenbankListeErstellen() {
+        BundesDatenbank florianWeiss = new BundesDatenbank("Florian", "Weiss", "blabla",
                 2, 11111, "Koeln", "deutsch", "00000", "13.01.1990",
                 "Hamburg");
-        BundesDatenbank vietNg = new BundesDatenbank("Viet", "Ng","blabla",
+        BundesDatenbank vietNg = new BundesDatenbank("Viet", "Ng", "blabla",
                 2, 11111, "Koeln", "deutsch", "00000", "13.01.1990",
                 "Hamburg");
-        BundesDatenbank oliverHock= new BundesDatenbank("Oliver", "Hock","blabla",
+        BundesDatenbank oliverHock = new BundesDatenbank("Oliver", "Hock", "blabla",
                 2, 11111, "Koeln", "deutsch", "00000", "13.01.1990",
                 "Hamburg");
-        BundesDatenbank peterMaier = new BundesDatenbank("Peter", "Maier","blabla",
+        BundesDatenbank peterMaier = new BundesDatenbank("Peter", "Maier", "blabla",
                 2, 11111, "Koeln", "deutsch", "00000", "13.01.1990",
                 "Hamburg");
-        BundesDatenbank peterWeiss = new BundesDatenbank("Peter", "Weiss","blabla",
+        BundesDatenbank peterWeiss = new BundesDatenbank("Peter", "Weiss", "blabla",
                 2, 11111, "Koeln", "deutsch", "00000", "13.01.1990",
                 "Hamburg");
         bundesDatenbankList.add(florianWeiss);
@@ -44,27 +46,27 @@ public class BackendTest {
         personendokument = new Personendokument();
 
     }
+
+
+
   /*
     Test der Klasse Validierung
      */
 
     @Test
-    public void pruefeEchtheitFalsch(){
+    public void pruefeEchtheitFalsch() {
         personendokument.setEchtheitsMerkmal("Führerschein");
         Assert.assertFalse(Validierung.pruefeAusweisEchtheit(personendokument));
     }
 
     @Test
-    public void pruefeEchtheitRichtig(){
+    public void pruefeEchtheitRichtig() {
         personendokument.setEchtheitsMerkmal("IDD");
         Assert.assertTrue(Validierung.pruefeAusweisEchtheit(personendokument));
     }
 
-
-
-
     @Test
-    public void pruefeDatenbankRichtig(){
+    public void pruefeDatenbankRichtig() {
         personendokument.setVorname("Florian");
         personendokument.setNachname("Weiss");
         Assert.assertTrue(Validierung.pruefeObRealePerson(personendokument, bundesDatenbankList));
@@ -72,27 +74,62 @@ public class BackendTest {
 
 
     @Test
-    public void pruefeDatenbankFalsch(){
+    public void pruefeDatenbankFalsch() {
         personendokument.setVorname("Florian");
         personendokument.setNachname("Müller");
         Assert.assertFalse(Validierung.pruefeObRealePerson(personendokument, bundesDatenbankList));
     }
 
 
-
+/*
+Tests der Klasse TesseractIdent
+ */
 
     @Test
-    public void leseTextAusRichtigerDateipfad(){
+    public void leseTextAusRichtigerDateipfad() {
         File file = new File("src\\main\\resources\\tesseract\\Muster_des_Personalausweises_RS.jpg");
         Assert.assertNotNull(TesseractIdent.leseTextaus(file));
     }
 
     @Test
-    public void leseTextAusFehlendenOderFehlerhaftenDateipfad(){
+    public void leseTextAusFehlendenOderFehlerhaftenDateipfad() {
         File file = new File("src\\main\\resources\\tesseract\\Nicht_Vorhanden.jpg");
         Assert.assertNull(TesseractIdent.leseTextaus(file));
     }
 
+/*
+Test der Klasse SubbildErsteller
+ */
 
+    @Test
+    public void erhalteRueckseiteOK() {
+        SubbildErsteller subbildErsteller = new SubbildErsteller();
+        Assert.assertNotNull(subbildErsteller.
+                erhalteRueckseite("src\\main\\resources\\tesseract\\Muster_des_Personalausweises_RS.jpg"));
+    }
 
+    @Test(expectedExceptions = AssertionError.class)
+    public void erhalteRueckseiteException() {
+        SubbildErsteller subbildErsteller = new SubbildErsteller();
+        Assert.assertNotNull
+            (subbildErsteller.
+                erhalteRueckseite("src\\main\\resources\\tesseract\\Nicht_vorhanden.jpg"));
+
+    }
+
+    @Test
+    public void erhalteVorderseiteOK() {
+        SubbildErsteller subbildErsteller= new SubbildErsteller();
+        Assert.assertNotNull(subbildErsteller.
+                erhalteVorderseite("src\\main\\resources\\tesseract\\Muster_des_Personalausweises_VS.jpg"));
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void erhalteVorderseiteException() {
+        SubbildErsteller subbildErsteller = new SubbildErsteller();
+        Assert.assertNotNull
+                (subbildErsteller.
+                        erhalteVorderseite("Nicht vorhanden"));
+
+    }
 }
