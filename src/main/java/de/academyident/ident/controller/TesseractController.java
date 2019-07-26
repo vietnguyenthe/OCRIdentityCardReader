d06e3afd8aa5ba465cb19b97e32fc4d94eba97a6
FlorianWeiss <git@florianweiss.dev>
2019-07-26 20:10:01 +0200

    OCR works with new image paths. Paths are dynamic.

--- src/main/java/de/academyident/ident/controller/TesseractController.java
+++ src/main/java/de/academyident/ident/controller/TesseractController.java
@@ -23,6 +23,13 @@ public class TesseractController {
     private static final String fileSeparator = System.getProperty("file.separator");
     private Path uploadFolder;
 
+
+    private String fileName;
+    private String fileName2;
+
+    private String frontImagePath;
+    private String backImagePath;
+
     @PostConstruct
     public void init() {
         String path = System.getProperty("user.home") + fileSeparator + "uploads";
@@ -40,7 +47,7 @@ public class TesseractController {
                                  @ModelAttribute("neueDokumentDaten") Personendokument dokument) throws IOException {
 
         MultipartFile backImage = tesseractFile.getBackImage();
-        String fileName = StringUtils.cleanPath(backImage.getOriginalFilename());
+        fileName = StringUtils.cleanPath(backImage.getOriginalFilename());
 
         InputStream inputStream = backImage.getInputStream();
         Files.copy(inputStream, uploadFolder.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
@@ -49,48 +56,45 @@ public class TesseractController {
 
 
         MultipartFile frontImage = tesseractFile.getFrontImage();
-        String fileName2 = StringUtils.cleanPath(frontImage.getOriginalFilename());
+        fileName2 = StringUtils.cleanPath(frontImage.getOriginalFilename());
 
         InputStream inputStream2 = frontImage.getInputStream();
         Files.copy(inputStream2, uploadFolder.resolve(fileName2), StandardCopyOption.REPLACE_EXISTING);
 
         model.addAttribute("targetFileName2", "readImage/" + fileName2);
 
+        frontImagePath = uploadFolder + fileSeparator + fileName2;
+        backImagePath = uploadFolder + fileSeparator + fileName;
+
+        // Create subImages of the original upload for OCR
+
+        SubbildErsteller subbildErsteller = new SubbildErsteller();
+
+        subbildErsteller.erstelleMaschinenlesbareZone(backImagePath);
+
+        String maschinenlesbareZone = TesseractIdent.leseTextaus(
+                new File("src\\main\\resources\\static\\img\\maschinenLesbareZone.jpg"));
+
+        subbildErsteller.erstelleAdresse(backImagePath);
+
+        String adresse = TesseractIdent.leseTextaus(
+                new File("src\\main\\resources\\static\\img\\adresse.jpg"));
+
+        subbildErsteller.erstelleGeburtsort(frontImagePath);
+
+        String geburtsort = TesseractIdent.leseTextaus(
+                new File("src\\main\\resources\\static\\img\\geburtsort.jpg"));
 
 
-//        tesseractFile.setPathFrontImage("../img/Perso_Front" + frontID + ".jpg");
-//        tesseractFile.setPathBackImage("Perso_Back" + backID + ".jpg");
-//
-//        SaveFile.saveFrontOnDisk(tesseractFile, "\\Perso_Front" + frontID + ".jpg");
-//        SaveFile.saveBackOnDisk(tesseractFile, "\\Perso_Back" + backID + ".jpg");
-//
-//        SubbildErsteller subbildErsteller = new SubbildErsteller();
-//
-//        subbildErsteller.erstelleMaschinenlesbareZone("src\\main\\resources\\static\\img\\Perso_Back" + backID + ".jpg");
-//
-//        String maschinenlesbareZone = TesseractIdent.leseTextaus(
-//                new File("src\\main\\resources\\static\\img\\maschinenLesbareZone.jpg"));
-//
-//        subbildErsteller.erstelleAdresse("src\\main\\resources\\static\\img\\Perso_Back" + backID + ".jpg");
-//
-//        String adresse = TesseractIdent.leseTextaus(
-//                new File("src\\main\\resources\\static\\img\\adresse.jpg"));
-//
-//        subbildErsteller.erstelleGeburtsort("src\\main\\resources\\static\\img\\Perso_Front" + frontID + ".jpg");
-//
-//        String geburtsort = TesseractIdent.leseTextaus(
-//                new File("src\\main\\resources\\static\\img\\geburtsort.jpg"));
-//
-//
-//        DeutscherAusweisOCR deutscherAusweisOCR = new DeutscherAusweisOCR(maschinenlesbareZone, adresse, geburtsort);
-//
-//        HashMap<String, String> ergebnisMap = deutscherAusweisOCR.getResultMap();
-//
-//        ocrModelMapping(dokument, ergebnisMap);
-//
-//        List<String> dateien = new ArrayList<>(Arrays.asList("src\\main\\resources\\static\\img\\adresse.jpg",
-//                                                             "src\\main\\resources\\static\\img\\geburtsort.jpg",
-//                                                             "src\\main\\resources\\static\\img\\maschinenLesbareZone.jpg"));
+        DeutscherAusweisOCR deutscherAusweisOCR = new DeutscherAusweisOCR(maschinenlesbareZone, adresse, geburtsort);
+
+        HashMap<String, String> ergebnisMap = deutscherAusweisOCR.getResultMap();
+
+        ocrModelMapping(dokument, ergebnisMap);
+
+        List<String> dateien = new ArrayList<>(Arrays.asList("src\\main\\resources\\static\\img\\adresse.jpg",
+                                                             "src\\main\\resources\\static\\img\\geburtsort.jpg",
+                                                             "src\\main\\resources\\static\\img\\maschinenLesbareZone.jpg"));
 //        LokaleBilddateien.loeschen(dateien);
 
         return "pruefung";
@@ -122,8 +126,35 @@ public class TesseractController {
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName2() {
+        return fileName2;
+    }
+
+    public void setFileName2(String fileName2) {
+        this.fileName2 = fileName2;
+    }
+
+    public String getFrontImagePath() {
+        return frontImagePath;
+    }
+
+    public String getBackImagePath() {
+        return backImagePath;
     }
 }
+
+
 
 
 // Diese Methonde kann die Brightness eines Bildes erhöhen. Der Float gibt dabei die % an (1.2 = 20%)
